f726072ca3dc0a14c02fffbcdc76b56e5dd35f04
THANH BÙI VĂN <18120559@student.hcmus.edu.vn>
2022-01-08 17:04:08 +0700

    update

--- adminshop/app.js
+++ adminshop/app.js
@@ -1,13 +1,27 @@
+// set up =======================================================
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const bodyParser = require('body-parser');
+//
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+var createError = require('http-errors');
 
 var app = express();
+
+// router files =================================================
+var indexRouter = require('./routes/index');
+  var adminRouter = require('./routes/admin');
+var Database = require('./DB/database');
+
+// Passport config ==============================================
+require('./config/passport')(passport);
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +32,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:'meo cats'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+// Static path
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/category', express.static(path.join(__dirname, 'public')));
+app.use('/store', express.static(path.join(__dirname, 'public')));
+app.use('/product', express.static(path.join(__dirname, 'public')));
+app.use('/order', express.static(path.join(__dirname, 'public')));
+app.use('/user', express.static(path.join(__dirname, 'public')));
+app.use('/login', express.static(path.join(__dirname, 'public')));
+
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global Vars - require cart controller
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Router
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,7 +87,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500);        
   res.render('error');
 });
 
